d387d17e765df40a1aa8a0d5d0832c378bfcbe82
Flo-Nz <63399869+Flo-Nz@users.noreply.github.com>
2021-02-03 16:46:04 +0100

    MDW Profile OK et route OK

--- floforum/my_modules/middlewares/profile.js
+++ floforum/my_modules/middlewares/profile.js
@@ -1,18 +1,47 @@
-const users = require('../database/users');
 
 const profile = {
-    //TODO function to verify if user is logged in. If true, call the display function. else, return an error "you do not have permission to access to this section. Please log in and retry".
 
+    findUser: (id, callback) => {
 
+        // In order to be sure id is a number, let parseint it
+        const userID = parseInt(id);
+        // Find the user informations in the database with its ID
+        const query = `SELECT * FROM user WHERE id = ${userID};`;
+
+        // The error or results will be managed by the callback function so we just pass it to the callback
+        client.query(query, (error, data) => {
+                callback(error, data);
+        });
+    },
+    
     display: (request, response) => {
 
-        const userID = parseInt(request.params.id);
-        
-        const userProfile = users.find(user => user.id === userID);
 
-        response.render('profile', {userProfile});
+        const userID = parseInt(request.params.id);
+
+        // Check if the user is Logged : if he is, call findUser function, if he is not, redirect him to the login page
+
+        if (request.session.login === true) {
+
+            profile.findUser(userID, (error, data) => {
+
+                // If everything is OK, render the myProfile page
+                if (error === null && data.rows.length > 0) {
+                    response.render('myProfile', {
+                        userInfo: data.rows[0],
+                    });
+                } 
+                // Else, redirect the user to the login page
+                else {
+                    response.redirect('/login');
+                }
+            });
+        }
+        else {
+            response.redirect('/login');
+        }
         
     }
-}
+};
 
 module.exports = profile;
